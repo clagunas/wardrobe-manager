78b20a444d78326abe5c4c452e76ff63058fdcfc
Cristina Lagunas Gualda <cristina.lagunas@desy.de>
2026-04-08 10:12:14 +0200

    single image bkg removal

--- remove_bkg.py
+++ remove_bkg.py
@@ -1,44 +1,86 @@
-#import cv2
+# import cv2
 import numpy as np
 import os
 from tqdm import tqdm
-#from skimage import io, color, morphology
+
+# from skimage import io, color, morphology
 from rembg import remove, new_session
 from PIL import Image
 from skimage import morphology
+import argparser
 
-
-input_folder = "preprocessed_images"
-output_folder = "output_images"
-
-os.makedirs(output_folder, exist_ok=True)
-
-#new
+# new
 session = new_session("u2net")
 
-for filename in tqdm(os.listdir(input_folder), desc="Processing images"):
 
-    if not filename.lower().endswith((".png", ".jpg", ".jpeg")):
-        continue
-
-    input_path = os.path.join(input_folder, filename)
-
+def remove_background(input_path, output_path):
     with Image.open(input_path) as img:
-        #result = remove(img)
         result = remove(
             img,
+            session=session,
             alpha_matting=True,
             alpha_matting_foreground_threshold=240,
             alpha_matting_background_threshold=10,
-            alpha_matting_erode_size=10
+            alpha_matting_erode_size=10,
         )
 
-    output_path = os.path.join(
-        output_folder,
-        os.path.splitext(filename)[0] + ".png"
-    )
-
     result.save(output_path)
+
+
+if "__main__" == __name__:
+    '''
+    Remove background from clothing item images. Can choose whether to process
+    the entire folder or just a single image.
+    '''
+
+    parser = argparser.ArgumentParser(
+        description="Remove background from clothing item images"
+    )
+    parser.add_argument(
+        "--input_folder",
+        type=str,
+        default="preprocessed_images",
+        help="Folder containing input images",
+    )
+    parser.add_argument(
+        "--output_folder",
+        type=str,
+        default="output_images",
+        help="Folder to save output images",
+    )
+    parser.add_argument(
+        "--image_filename",
+        type=str,
+        help="Filename of a single image to process (if not provided, processes all images in the input folder)",
+    )
+    args = parser.parse_args()
+
+    os.makedirs(args.output_folder, exist_ok=True)
+
+    if args.image_filename:
+
+        if not args.image_filename.lower().endswith((".png", ".jpg", ".jpeg")):
+            print("Invalid image file format.")
+            exit(1)
+        input_path = os.path.join(args.input_folder, args.image_filename)
+        output_path = os.path.join(
+            args.output_folder, os.path.splitext(args.image_filename)[0] + ".png"
+        )
+        remove_background(input_path, output_path)
+
+    else:
+
+        for filename in tqdm(os.listdir(args.input_folder), desc="Processing images"):
+
+            if not filename.lower().endswith((".png", ".jpg", ".jpeg")):
+                continue
+
+            input_path = os.path.join(args.input_folder, filename)
+            output_path = os.path.join(
+                args.output_folder, os.path.splitext(filename)[0] + ".png"
+            )
+
+            remove_background(input_path, output_path)
     # path = os.path.join(input_folder, filename)
 
     # img = Image.open(path).convert("RGB")
@@ -75,7 +117,6 @@ for filename in tqdm(os.listdir(input_folder), desc="Processing images"):
     # )
 
     # Image.fromarray(rgba).save(output_path)
-
 
 
 print("Background removal complete!")
